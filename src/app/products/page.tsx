c755f085fb6e9f3c56d83c2112e00bdf1bb6cd70
Sonu Kumar <1AT20IS092@visioneer.atria.edu>
2024-06-23 23:09:40 +0530

    Update

---
+++ src/app/products/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ProductPage: React.FC = () => {
+    return (
+        <div>
+            <h1>Product Page</h1>
+            {/* Add your product page content here */}
+        </div>
+    );
+};
+
+export default ProductPage;
